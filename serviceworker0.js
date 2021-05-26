b23d512ccee717ed54c82f8ea1a4a25142317f8a
AnsifDev <84790890+AnsifDev@users.noreply.github.com>
2021-05-26 07:56:39 +0530

    Update serviceworker0.js

--- serviceworker0.js
+++ serviceworker0.js
@@ -2,10 +2,10 @@ self.addEventListener("install", e=>{
     alert("ServiceWorker first load suceeded");
     e.waitUntil(caches.open("static").then(cache=>{
         return cache.addAll(["./begin.html", "./img1.png", "./index.html"]);
-    }))
-})
+    }));
+});
 self.addEventListener("fetch", e=>{
     caches.match(e.request).then(req=>{
         return req || e request;
-    })
-})
+    });
+});
